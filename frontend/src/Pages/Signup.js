9f4f43f22b221f3d1820f7d6180b706800e1b16d
Colin Lawrence <lawrencec1@ufl.edu>
2024-03-06 13:18:26 -0500

    I added the login and signup frontend pages

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 function Signup() {
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className ="text-center mt-5" style={{ maxWidth: "400px", margin: "0 auto" }}>
+      <h1>Please Sign Up</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="emailAddress" class="form-control" 
+      placeholder = "Email Address" required autoFocus></input>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
+      <div className ="mt-3">
+        <button className ="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </div>
     </div>
   );
 }
