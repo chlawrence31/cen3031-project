6a61846198f543c086a73c1f6fb9ce8ec4473911
s-trejo <strejo9811@gmail.com>
2024-03-06 03:30:34 -0500

    Showcase slider on about page

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import AnimalSlider from '../Components/AnimalSlider'; // Check this line to ensure the correct path
 
-export default class About extends Component{
+export default class About extends Component {
     render() {
         return (
             <div>
                 <h1>About</h1>
+                <AnimalSlider />
             </div>
-        )
+        );
     }
 }
