300cf493f1deefb8df889bdd85486ae959159634
s-trejo <strejo9811@gmail.com>
2024-03-06 05:19:26 -0500

    Linked login & signup to btn

--- frontend/src/Components/LoginSignUpButton.js
+++ frontend/src/Components/LoginSignUpButton.js
@@ -1,11 +1,12 @@
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { Link } from 'react-router-dom';
 
 function LoginSignUpButton() {
   return (
     <ButtonGroup aria-label="Basic example" size="sm">
-      <Button variant="outline-primary" style={{fontWeight: 'bold'}}>Login</Button>
-      <Button variant="success" style={{fontWeight: 'bold'}}>Sign-Up</Button>
+      <Button as={Link} to="/login" variant="outline-primary" style={{fontWeight: 'bold'}}>Login</Button>
+      <Button  as={Link} to="/signup" variant="success" style={{fontWeight: 'bold'}}>Sign-Up</Button>
     </ButtonGroup>
   );
 }
