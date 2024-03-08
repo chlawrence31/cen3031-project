6f8db706baa3bed81ec85881d641514065049b58
Colin Lawrence <lawrencec1@ufl.edu>
2024-03-08 15:11:38 -0500

    Started work on the serverside login setup

--- backend/server.js
+++ backend/server.js
@@ -25,4 +25,53 @@ app.get("/animals", (req,res) => {
     })
 })
 
+//Checks if user is logged in
+app.get('/', (req, res) => 
+{
+    if(req.session.username)
+    {
+        return res.json({valid: true, username: req.session.username})
+    }
+    else
+    {
+        return res.json({valid: false})
+    }
+})
+
+//Handles signup requests
+app.post('/signup', (req, res) => 
+{
+    const sql = "Insert user values"
+    const values = 
+    [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values], (err, result) => 
+    {
+        if(err) return res.json({Message: "Error in inserting user values"})
+        return res.json.result
+    })
+})
+
+//Handles login requests
+app.post('/login', (req, res) => 
+{
+    const sql = "Selecting users from email"
+    db.query(sql, [req.body.email, req.body.password], (err, result) => 
+    {
+        if(err) return res.json({Message: "Error inside server"});
+        if(result.length > 0)
+        {
+            req.session.username = result[0].username;
+            return res.json({Login: true})
+        }
+        else
+        {
+            return res.json({Login: false})
+        }
+    })
+})
+
 app.listen(5000, ()=> {console.log("Server started on port 5000")})
