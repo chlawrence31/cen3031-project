b12967a5780cd3f1c0fc872e1da82ceeff1ffbee
s-trejo <strejo9811@gmail.com>
2024-03-08 14:44:44 -0500

    animal table req

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mysql = require('mysql2')
+const cors = require('cors')
 
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+    host:"local host",
+    user: "root",
+    password: "",
+    database: ""        // Update to the name of your particular database set in mysqlWorkbench
+})
 app.get("./api", (req, res) => 
 {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
+// Function to get the 100 animals from the animals table in the mysql db
+app.get("/animals", (req,res) => {
+    const sql = "SELECT * FROM animals";
+    db.query(sql,(err, data)=> {
+        if(err) return res.json(err);
+        return res.json(data)
+    })
 })
 
 app.listen(5000, ()=> {console.log("Server started on port 5000")})
