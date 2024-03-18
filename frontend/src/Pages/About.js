77d3733ea289cf343782b8f647b7ef529f6bff0a
bryan_bong <bryanbong38@gmail.com>
2024-03-18 15:50:55 -0400

    Added more text to About Page

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -42,6 +42,17 @@ function About() {
         </Swiper>
 
         <h2 style={{ textAlign: "center" }}>Dive Deeper</h2>
+
+        <p>Welcome to Big Cat Rescue, a dedicated organization committed to the preservation and protection of exotic wildcat species. Founded with a passion for wild cat protection and conservation, we have been at the forefront of critical efforts to safeguard these remarkable creatures for over 31 years.</p>
+
+        <p>Our journey began by recognizing the urgent need to address the issues faced by big cats in captivity. Through tireless advocacy and collaboration, we played a pivotal role in the passing of the U.S. federal Big Cat Public Safety Act in December 2022. This landmark legislation stands as a testament to our unwavering commitment to the well-being of big cats across the nation.</p>
+
+        <p>While our efforts in securing legislation are pivotal, we believe that true conservation lies beyond the walls of captivity. To ensure the long-term survival of wildcat species, we have turned our attention to protecting them in their natural habitats. By funding insitu projects, we empower local communities and conservation initiatives to combat the various threats faced by wildcats, including the perils of the pet trade, wildlife trafficking, bushmeat and sport hunting, as well as the collection of these magnificent creatures as zoo specimens.</p>
+
+        <p>At Big Cat Rescue we are driven by a shared vision of a future where wildcats roam freely in their natural habitats, unhindered by human interference. We recognize the intrinsic value of these awe-inspiring creatures and the pivotal role they play in maintaining the delicate balance of our ecosystems. Through collaboration, education, and unwavering dedication, we strive to protect and preserve the irreplaceable legacy of wildcats for generations to come.</p>
+
+        <p>Join us in our mission to make a meaningful impact on the lives of these extraordinary animals. Our Goal is for our donors to find their gifts to us to be among the most satisfying they make by accomplishing our mission in the most financially responsible manner possible. We consistently receive Charity Navigator’s highest 4 star rating. Together, we can create a world where wildcats thrive, where they inspire awe and respect, and where their existence serves as a testament to the beauty and diversity of our planet.</p>
+ 
         <p>We’re proud to have the endorsement of celebrities, the backing of multiple awards, 
           and extensive media coverage. We maintain complete transparency, sharing our finances 
           and operations, so you can be confident that your support is put to the best possible use.</p>
