e55130c410aac5d73a7e5ca607bbaeb72a9281c6
bryan_bong <bryanbong38@gmail.com>
2024-03-21 17:52:01 -0400

    Additions to homepage text. Changed image sizes.

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -11,10 +11,20 @@ function HomePage() {
         <h1 style={{ textAlign: "center" }}>Mission Statement</h1>
         <h3 style={{ textAlign: "center" }}>Our mission is to safeguard the world’s exotic wildcat species through a multifaceted approach. Make your contribution today!</h3>
       </div>
+
+      <div style = {{ fontFamily: 'Copperplate, serif', fontSize: '20px'}}>
+        <p>With an unwavering commitment to conservation, we strive to protect these majestic creatures both in captivity and in their natural habitats.&nbsp;</p>
+
+        <p>By championing the Big Cat Public Safety Act and funding insitu projects, we work tirelessly to combat the threats posed by the pet trade, wildlife trafficking, bushmeat and sport hunting, as well as the collection of wildcats for zoos.&nbsp;</p>
+
+        <p>Our ultimate goal is to <strong>ensure the survival and thriving future of wildcats</strong>, fostering a world where they can roam freely and their presence continues to enrich our planet.</p>
+      </div>
+
       <div style = {{ fontFamily: 'Copperplate, serif' }}>
         <h2 style={{ textAlign: "center" }}>Meet Our Most Popular Animals</h2>
         <TopAnimalSlides topAnimals={topAnimals} />
       </div>
+
     </div>
   );
 }
--- frontend/src/Pages/TopAnimalSlides.js
+++ frontend/src/Pages/TopAnimalSlides.js
@@ -20,7 +20,7 @@ export const TopAnimalSlides = ({ topAnimals }) => {
             {topAnimals.map((slide) => (
                 <SwiperSlide style={{ backgroundPosition: 'center', backgroundSize: 'cover' , 
                 background: '#1A202C', color: 'white', fontFamily: 'Copperplate, serif'}} key={slide.image} effect>
-                    <img style= {{ width: 400, height: 500 }} src={slide.image} alt={slide.title}/>
+                    <img style= {{ width: 490, height: 500 }} src={slide.image} alt={slide.title}/>
                     <h3>Name: {slide.name}</h3>
                     <h3>Species: {slide.species}</h3>
                     <h3>Sex: {slide.sex}</h3>
