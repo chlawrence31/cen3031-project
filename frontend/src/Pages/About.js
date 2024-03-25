1bbf42905745cacbed73fbc8981e89a9ad328779
bryan_bong <bryanbong38@gmail.com>
2024-03-24 22:21:09 -0400

    Added a lot of text to home and about pages.

--- frontend/src/Pages/About.js
+++ frontend/src/Pages/About.js
@@ -4,7 +4,7 @@ import AboutBanner from '../Components/AboutBanner';
 
 function About() {
     return (
-      <div>
+      <div style = {{ fontFamily: 'Copperplate, serif', fontSize: '20px'}}>
         <AboutBanner />
         <Swiper 
           spaceBetween={30}
@@ -43,20 +43,33 @@ function About() {
 
         <h2 style={{ textAlign: "center" }}>Dive Deeper</h2>
 
-        <p>Welcome to Big Cat Rescue, a dedicated organization committed to the preservation and protection of exotic wildcat species. Founded with a passion for wild cat protection and conservation, we have been at the forefront of critical efforts to safeguard these remarkable creatures for over 31 years.</p>
-
-        <p>Our journey began by recognizing the urgent need to address the issues faced by big cats in captivity. Through tireless advocacy and collaboration, we played a pivotal role in the passing of the U.S. federal Big Cat Public Safety Act in December 2022. This landmark legislation stands as a testament to our unwavering commitment to the well-being of big cats across the nation.</p>
-
-        <p>While our efforts in securing legislation are pivotal, we believe that true conservation lies beyond the walls of captivity. To ensure the long-term survival of wildcat species, we have turned our attention to protecting them in their natural habitats. By funding insitu projects, we empower local communities and conservation initiatives to combat the various threats faced by wildcats, including the perils of the pet trade, wildlife trafficking, bushmeat and sport hunting, as well as the collection of these magnificent creatures as zoo specimens.</p>
-
-        <p>At Big Cat Rescue we are driven by a shared vision of a future where wildcats roam freely in their natural habitats, unhindered by human interference. We recognize the intrinsic value of these awe-inspiring creatures and the pivotal role they play in maintaining the delicate balance of our ecosystems. Through collaboration, education, and unwavering dedication, we strive to protect and preserve the irreplaceable legacy of wildcats for generations to come.</p>
-
-        <p>Join us in our mission to make a meaningful impact on the lives of these extraordinary animals. Our Goal is for our donors to find their gifts to us to be among the most satisfying they make by accomplishing our mission in the most financially responsible manner possible. We consistently receive Charity Navigator’s highest 4 star rating. Together, we can create a world where wildcats thrive, where they inspire awe and respect, and where their existence serves as a testament to the beauty and diversity of our planet.</p>
+        <p>Big Cat Rescue, one of the world’s most influential accredited sanctuaries for exotic cats, and a leading advocate in ending the abuse of captive big cats and saving wild cats from extinction.  We are home to tigers, jaguar, bobcats, servals and other species most of whom have been abandoned, abused, orphaned, saved from being turned into fur coats, or retired from performing acts.</p>
  
+        <p>Big Cat Rescue was founded in November of 1992 and is a non-profit 501(c)3 charity. The sanctuary is situated on 67 acres in the Citrus Park area of north Tampa, Florida.</p>
+
+        <p>The cats residing at Big Cat Rescue are here for a variety of reasons, including:</p>
+        <ul>
+          <li>Abandoned by owners who wrongly thought they would make good pets</li>
+          <li>Abused by owners in order to force them to perform</li>
+          <li>Retired from performing acts</li>
+          <li>Saved from being slaughtered to make fur coats</li>
+          <li>Rescued as babies after hunters killed their mothers.</li>  
+        </ul>
+
+        <p>Big Cat Rescue is:</p>
+        <ul>
+          <li>Accredited by the Global Federation of Sanctuaries</li>
+          <li>Certified by Independent Charities of America as a “Best in America Charity”</li>
+          <li>Member of the World Society for Protection of Animals</li>
+          <li>Rated 4 Stars by Charity Navigator (their highest rating) and has one of the highest scores of any animal based charity</li>
+          <li>Part of a global coalition including HSUS, IFAW, WWF, GFAS, Born Free and other animal protection groups who are working together to end big cat abuse.</li>
+        </ul>
+
         <p>We’re proud to have the endorsement of celebrities, the backing of multiple awards, 
           and extensive media coverage. We maintain complete transparency, sharing our finances 
           and operations, so you can be confident that your support is put to the best possible use.</p>
       </div>
+      
     );
   }
   
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -23,9 +23,23 @@ function HomePage() {
       <div style = {{ fontFamily: 'Copperplate, serif' }}>
         <h2 style={{ textAlign: "center" }}>Meet Our Most Popular Animals</h2>
         <TopAnimalSlides topAnimals={topAnimals} />
+        <h2 style={{ textAlign: 'center'}}>Our Goals</h2>
+      </div>
+
+      <div style = {{ fontFamily: 'Copperplate, serif', fontSize: '20px'}}>
+        <p>Welcome to Big Cat Rescue, a dedicated organization committed to the preservation and protection of exotic wildcat species. Founded with a passion for wild cat protection and conservation, we have been at the forefront of critical efforts to safeguard these remarkable creatures for over 31 years.</p>
+
+        <p>Our journey began by recognizing the urgent need to address the issues faced by big cats in captivity. Through tireless advocacy and collaboration, we played a pivotal role in the passing of the U.S. federal Big Cat Public Safety Act in December 2022. This landmark legislation stands as a testament to our unwavering commitment to the well-being of big cats across the nation.</p>
+
+        <p>While our efforts in securing legislation are pivotal, we believe that true conservation lies beyond the walls of captivity. To ensure the long-term survival of wildcat species, we have turned our attention to protecting them in their natural habitats. By funding insitu projects, we empower local communities and conservation initiatives to combat the various threats faced by wildcats, including the perils of the pet trade, wildlife trafficking, bushmeat and sport hunting, as well as the collection of these magnificent creatures as zoo specimens.</p>
+
+        <p>At Big Cat Rescue we are driven by a shared vision of a future where wildcats roam freely in their natural habitats, unhindered by human interference. We recognize the intrinsic value of these awe-inspiring creatures and the pivotal role they play in maintaining the delicate balance of our ecosystems. Through collaboration, education, and unwavering dedication, we strive to protect and preserve the irreplaceable legacy of wildcats for generations to come.</p>
+
+        <p>Join us in our mission to make a meaningful impact on the lives of these extraordinary animals. Our Goal is for our donors to find their gifts to us to be among the most satisfying they make by accomplishing our mission in the most financially responsible manner possible. We consistently receive Charity Navigator’s highest 4 star rating. Together, we can create a world where wildcats thrive, where they inspire awe and respect, and where their existence serves as a testament to the beauty and diversity of our planet.</p>
       </div>
 
     </div>
+
   );
 }
 
