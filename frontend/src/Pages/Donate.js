bcbdc0bbc0bdd80f665c24ebef29a04156520f17
nguyen-stephanie <nguyen.stephanie@ufl.edu>
2024-03-27 11:11:48 -0400

    Updated donate.js
    Added fixed donation and custom amount buttons to the donate page

--- frontend/src/Pages/Donate.js
+++ frontend/src/Pages/Donate.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import DonationBanner from '../Components/DonationBanner';
 import { Link } from 'react-router-dom';
 
 function Donate() {
+  const [donationAmount, setDonationAmount] = useState();
+
+  const handleAmountChange = (event) => {
+    setDonationAmount(parseFloat(event.target.value));
+  }
+
   return (
       <div>
         <DonationBanner/>
         <div className = 'mt-5'>
           <h1 className="text-center">Donate to Big Cat Rescue</h1>
           <div style={{ maxWidth: "400px", margin: "0 auto" }}>
-          <input type="number" id="donationAmount" class="form-control mb-3" min="0" required 
-          placeholder = "$ Donation amount"></input>
-          <div className ="text-center mt-3">
-          <Link to="/donationlogin" className ="btn btn-lg btn-primary btn-block" type="submit" style={{ marginBottom: '100px'}}>Donate</Link>
-        </div>
+          <div className="text-center mt-3" style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+            <button onClick={() => setDonationAmount(5)} className="btn btn-lg btn-primary mr-2">$5</button>
+            <button onClick={() => setDonationAmount(10)} className="btn btn-lg btn-primary mr-2">$10</button>
+            <button onClick={() => setDonationAmount(25)} className="btn btn-lg btn-primary mr-2">$25</button>
+            <button onClick={() => setDonationAmount(50)} className="btn btn-lg btn-primary mr-2">$50</button>
+            <button onClick={() => setDonationAmount(100)} className="btn btn-lg btn-primary mr-2">$100</button>
+            </div>
+            <div className="text-center mt-3" style={{marginTop: "10px"}}>
+              <input type="number" id="donationAmount" class="form-control mb-3" min="0" value={donationAmount} onChange={handleAmountChange} required 
+            placeholder = "$ Custom Donation amount"></input>
+            <Link to="/donationlogin" className ="btn btn-lg btn-primary btn-block" type="submit" style={{ marginBottom: '100px'}}>Donate</Link>
+            </div>
+          
           </div>
       </div>
 
