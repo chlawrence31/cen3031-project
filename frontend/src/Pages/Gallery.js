2c4f8263d129b2831a1a6d77d37b3b83bfc3c7db
s-trejo <strejo9811@gmail.com>
2024-04-10 03:34:48 -0400

    Updated Gallery

--- frontend/src/Pages/Gallery.js
+++ frontend/src/Pages/Gallery.js
@@ -29,12 +29,12 @@ const Gallery = () => {
 
   return (
     <div className="container">
-      <h1>Animal Gallery</h1>
+      <h1 className="mt-4 mb-4" style={{ fontSize: '3rem' }}>Animal Gallery</h1>
       {animals.length > 0 ? (
         <Row>
           {animals.map((animal) => (
-            <Col key={animal.ID} xs={12} sm={6} md={4} lg={3}>
-              <Card style={{ height: '100%', position: 'relative' }}>
+            <Col key={animal.ID} xs={12} sm={6} md={4} lg={3} style={{ marginBottom: '30px' }}>
+              <Card style={{ height: '100%', position: 'relative', boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.8)' }}>
                 {animal.Image && (
                   <Card.Img
                     variant="top"
@@ -43,15 +43,15 @@ const Gallery = () => {
                     style={{ height: '200px', objectFit: 'cover' }} // Adjust dimensions as needed
                   />
                 )}
-                <Card.Body style={{ height: '300px' }}>
-                  <Card.Title style={{ minHeight: '50px' }}>{animal.Name}</Card.Title>
+                <Card.Body style={{ height: '260px', marginBottom: '0' }}>
+                  <Card.Title style={{ marginBottom: '3%' }}>{animal.Name}</Card.Title>
                   <Card.Text style={{ minHeight: '100px' }}>
-                    <p>Species: {animal.Species}</p>
-                    <p>Sex: {animal.Sex}</p>
-                    <p>{animal.Description}</p>
+                  <div style={{ marginTop: '0', marginBottom: '0', lineHeight: '1.2'}}>Species: {animal.Species}</div>
+                  <div style={{ marginTop: '0', marginBottom: '5%', lineHeight: '1.2'}}>Sex: {animal.Sex}</div>
+                  <div style={{ lineHeight: '1.2' }}>{animal.Description}</div>
                   </Card.Text>
                   {/* Pass animal data to handleDonateClick */}
-                  <button className="btn btn-primary" style={{ position: 'absolute', bottom: '5px' }} onClick={() => handleDonateClick(animal)}>Donate</button>
+                  <button className="btn btn-primary" style={{ position: 'absolute', bottom: '15px' }} onClick={() => handleDonateClick(animal)}>Donate</button>
                 </Card.Body>
               </Card>
             </Col>
